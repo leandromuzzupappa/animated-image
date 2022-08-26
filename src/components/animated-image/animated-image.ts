b131c25a2eac0a5b3c089f3402200805b5257035
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2022-08-26 00:48:03 -0300

    :sparkles:

--- src/components/animated-image/animated-image.ts
+++ src/components/animated-image/animated-image.ts
@@ -1,5 +1,6 @@
 import { LitElement, html } from "lit";
 import { customElement } from "lit/decorators.js";
+import gsap from "gsap";
 
 import { styles } from "./styles";
 import img_default from "@assets/images/img_default.svg";
@@ -7,6 +8,71 @@ import img_default from "@assets/images/img_default.svg";
 @customElement("animated-image")
 export class AnimatedImage extends LitElement {
   static styles = styles;
+
+  constructor() {
+    super();
+    console.log("constructor");
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback();
+
+    console.log("connected callback", this.getElements().image);
+    this.addEventListener("mousemove", this.handleMouseMove);
+    this.addEventListener("mouseleave", this.handleReset);
+  }
+
+  firstUpdated() {
+    console.log("firstUpdated", this.getElements().image);
+  }
+
+  updated() {
+    console.log("updated", this.getElements().image);
+  }
+
+  handleMouseMove(event) {
+    const { image, squares } = this.getElements();
+    if (!image) return;
+
+    const { clientWidth, clientHeight } = event.target;
+    const { offsetX, offsetY } = event;
+    const x = Math.abs(offsetX / 10) - clientWidth / 20;
+    const y = Math.abs(offsetY / 10) - clientHeight / 20;
+
+    squares.forEach((square, index) => {
+      const squareX = x * 0.25 * (index === 0 ? 1 : -4);
+      const squareY = y * 0.25 * (index === 0 ? 1 : -4);
+
+      square.style.transform = `translate(${squareX}px, ${squareY}px)`;
+    });
+
+    this.style.setProperty("--position-x", `${x}px`);
+    this.style.setProperty("--position-y", `${y}px`);
+    this.style.setProperty("--square2--clr-1", offsetX);
+    this.style.setProperty("--square2--clr-2", offsetY);
+  }
+
+  handleReset() {
+    this.style.setProperty("--position-x", "0");
+    this.style.setProperty("--position-y", "0");
+    this.style.setProperty("--square2--clr-1", "12");
+    this.style.setProperty("--square2--clr-2", "250");
+
+    const { squares } = this.getElements();
+    if (!squares) return;
+
+    squares.forEach((square) => {
+      square.style.transform = `translate(0px, 0px)`;
+    });
+  }
+
+  getElements() {
+    return {
+      component: this.renderRoot,
+      image: this.renderRoot.querySelector<this>("img"),
+      squares: this.renderRoot.querySelectorAll<HTMLDivElement>(".squares"),
+    };
+  }
 
   render() {
     return html` <div class="animated-title">
--- src/components/animated-image/styles.ts
+++ src/components/animated-image/styles.ts
@@ -4,7 +4,12 @@ export const styles = css`
   :host {
     --squares-bradius: 20px;
     --squares-bg: linear-gradient(180deg, #5453d0 0%, #317cfa 100%);
-    --squares-bg2: linear-gradient(to bottom, #8f94fb, #4e54c8);
+
+    --squares-bg2: linear-gradient(
+      to bottom,
+      hsl(var(--square2--clr-1, 12), 50%, 50%),
+      hsl(var(--square2--clr-2, 250), 50%, 50%)
+    );
 
     position: relative;
     display: grid;
@@ -19,6 +24,8 @@ export const styles = css`
     width: 100%;
     border: 0;
     z-index: 1;
+    translate: var(--position-x, 0) var(--position-y, 0);
+    transform-origin: center;
   }
 
   .squares {
@@ -32,12 +39,15 @@ export const styles = css`
     background: var(--squares-bg);
     border-radius: var(--squares-bradius);
     opacity: 0.8;
+    transform-origin: center;
   }
 
   .squares:nth-child(2) {
     top: -30px;
     left: 60%;
     width: 70%;
+    translate: -50% 0%;
     background: var(--squares-bg2);
+    box-shadow: 0px 0px 16px var(--squares-bg);
   }
 `;
