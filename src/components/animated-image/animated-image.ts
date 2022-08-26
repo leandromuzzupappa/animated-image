809707cfe173e1979714e8d4db6bf085c19a0bcc
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2022-08-25 21:13:05 -0300

    Basic markup and some styles. Also added an image

--- src/components/animated-image/animated-image.ts
+++ src/components/animated-image/animated-image.ts
@@ -1,13 +1,19 @@
 import { LitElement, html } from "lit";
 import { customElement } from "lit/decorators.js";
+
 import { styles } from "./styles";
+import img_default from "@assets/images/img_default.svg";
 
 @customElement("animated-image")
 export class AnimatedImage extends LitElement {
   static styles = styles;
 
   render() {
-    return html` <h1>Lenny</h1> `;
+    return html` <div class="animated-title">
+      <img src=${img_default} alt="default" />
+      <div class="squares"></div>
+      <div class="squares"></div>
+    </div>`;
   }
 }
 
--- src/components/animated-image/styles.ts
+++ src/components/animated-image/styles.ts
@@ -2,5 +2,42 @@ import { css } from "lit";
 
 export const styles = css`
   :host {
+    --squares-bradius: 20px;
+    --squares-bg: linear-gradient(180deg, #5453d0 0%, #317cfa 100%);
+    --squares-bg2: linear-gradient(to bottom, #8f94fb, #4e54c8);
+
+    position: relative;
+    display: grid;
+    place-items: center;
+    width: 500px;
+    aspect-ratio: 1/1;
+  }
+
+  img {
+    position: relative;
+    display: block;
+    width: 100%;
+    border: 0;
+    z-index: 1;
+  }
+
+  .squares {
+    position: absolute;
+    inset: 0;
+    left: 50%;
+    translate: -50% 0%;
+    display: block;
+    width: 80%;
+    height: 100%;
+    background: var(--squares-bg);
+    border-radius: var(--squares-bradius);
+    opacity: 0.8;
+  }
+
+  .squares:nth-child(2) {
+    top: -30px;
+    left: 60%;
+    width: 70%;
+    background: var(--squares-bg2);
   }
 `;
